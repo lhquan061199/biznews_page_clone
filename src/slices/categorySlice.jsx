d5615e5cb3ce88d59682a508110484b48cbedf5f
lequan <lequan0611@gmail.com>
2023-09-07 20:42:01 +0700

    fix/4-homepage-layout-navbar

--- src/slices/categorySlice.jsx
+++ src/slices/categorySlice.jsx
@@ -4,21 +4,11 @@ import { getCategory } from "../service/categoriesService";
 const categorySlice = createSlice({
   name: "category",
   initialState: {
-    a: [],
     loading: false,
     error: "",
   },
   reducer: {},
   extraReducers: {
-    // [getCategory.pending]: (state) => {
-    //     state.loading = true
-
-    // },
-    // [getCategory.rejected]:(state) => {
-    //     state.loading = false
-    //     state.error = ''
-
-    // },
     [getCategory.fulfilled]: (state, action) => {
       state.loading = false;
       state.categories = action.payload;
